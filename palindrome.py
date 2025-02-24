fd3182e1aa42286b4a24039b9873a2a9cdfd4e73
sirisha-03 <gourisirisha2000@gmail.com>
2025-02-23 20:51:20 -0800

    Question_2

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+# Given a string s, your task is to find the longest palindromic substring within s.
+#Input: s= "aaaabbaa"
+#Output: "aabbaa"
+#Explanation: The longest palindromic substring is "aabbaa".
+
+def palindrome(n):
+    return 0
